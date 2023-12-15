641c780095c3d27a2b6b8676d4af7ff5646c0490
CedricDsmet <Cedric.de.smet@outlook.com>
2023-12-15 09:31:33 +0100

    scroll effect on article page

--- pages/news/[...slug].vue
+++ pages/news/[...slug].vue
@@ -2,7 +2,7 @@
   <main>
     <ContentDoc v-slot="{ doc }">
       <div
-        class="hero min-h-[80vh] z-10"
+        class="hero min-h-[80vh] fixed z-10"
         :style="`background-image: url(${doc.image}); background-size: cover`"
       >
         <div class="hero-overlay bg-opacity-60"></div>
@@ -14,9 +14,10 @@
           </div>
         </div>
       </div>
+      <div class="min-h-[80vh] min-w-full"></div>
       <div class="relative flex flex-wrap justify-center z-2 bg-base-300">
         <div
-          class="bg-base-100 shadow-md md:basis-9/12 basis-11/12 shrink-0 grow-0 rounded mb-20"
+          class="bg-base-100 shadow-md md:basis-9/12 basis-11/12 shrink-0 grow-0 z-20 rounded mb-20"
           style="margin-top: -12em"
         >
           <div class="p-10">
