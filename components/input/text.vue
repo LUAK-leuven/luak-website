3d4835da7571fcd006fc3cd85d1cfa23f5dac5bf
cedricDsmet <cedric.de.smet@outlook.com>
2024-05-22 10:38:13 +0000

    add extra fields and supabase to signup form

---
+++ components/input/text.vue
@@ -0,0 +1,36 @@
+<template>
+  <label class="form-control w-full mb-2">
+    <div class="label">
+      <span class="label-text">{{ label }}</span>
+    </div>
+    <Field
+      :name="name"
+      :type="type"
+      :placeholder="placeholder"
+      class="input input-bordered w-full"
+    />
+    <ErrorMessage as="span" class="text-error" :name="name" />
+  </label>
+</template>
+
+<script setup>
+import { Field } from "vee-validate";
+defineProps({
+  label: {
+    type: String,
+    default: "text",
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  name: {
+    type: String,
+    default: "text",
+  },
+  placeholder: {
+    type: String,
+    default: "text",
+  },
+});
+</script>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,57 +1,86 @@
 <script setup lang="ts">
-const config = useRuntimeConfig();
+import * as yup from "yup";
+import { Form } from "vee-validate";
+
 const supabase = useSupabaseClient();
-const email = ref("");
-const password = ref("");
+const phoneRegExp = /^[+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
 
-const signUpWithPassword = async () => {
-  validateEmail(email.value);
+const formSchema = yup.object({
+  firstName: yup.string().required().label("First name"),
+  lastName: yup.string().required().label("Last name"),
+  email: yup.string().required().email(),
+  password: yup.string().required().min(5),
+  phoneNumber: yup
+    .string()
+    .matches(phoneRegExp, "Format of the phone number is incorrect."),
+});
+interface formValues extends yup.InferType<typeof formSchema> {}
 
-  const { data, error } = await supabase.auth.signUp({
-    email: email.value,
-    password: password.value,
+async function onSubmit({ email, password, firstName, lastName }: formValues) {
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        firstName,
+        lastName,
+      },
+    },
   });
   if (error) console.log(error);
-};
+}
 </script>
 <template>
   <div class="relative flex flex-wrap justify-center z-2 bg-base-300">
     <div
       class="bg-base-100 shadow-md rounded w-10/12 lg:w-8/12 xl:w-1/3 mb-28 z-10 mt-72 p-20"
     >
-      <div>
-        <label class="form-control w-full">
-          <div class="label">
-            <span class="label-text">Email</span>
-          </div>
-          <input
-            v-model="email"
-            type="email"
-            placeholder="youremail@example.com"
-            class="input input-bordered w-full"
+      <Form
+        v-slot="{ isSubmitting }"
+        :validation-schema="formSchema"
+        @submit="onSubmit"
+      >
+        <div class="flex flex-row justify-stretch">
+          <InputText
+            label="First name"
+            name="firstName"
+            placeholder="Alex"
+            class="mr-1"
           />
-        </label>
-        <label class="form-control w-full">
-          <div class="label">
-            <span class="label-text">Password</span>
-          </div>
-          <input
-            v-model="password"
-            type="password"
-            placeholder="******"
-            class="input input-bordered w-full"
+          <InputText
+            label="Last name"
+            name="lastName"
+            placeholder="Megos"
+            class="ml-1"
           />
-        </label>
-        <InputPhone v-model="phoneNumber"></InputPhone>
+        </div>
+        <InputText
+          label="Email"
+          name="email"
+          placeholder="youremail@example.com"
+          type="email"
+        />
+        <InputText
+          label="Password"
+          name="password"
+          placeholder="*******"
+          type="password"
+        />
+        <InputText
+          label="Phone Number (for WhatsApp)"
+          name="phoneNumber"
+          placeholder="+32468123123"
+          type="tel"
+        />
         <div class="flex justify-center">
-          <button
-            class="btn btn-primary w-full m-5"
-            @click="signUpWithPassword"
-          >
-            Sign up
+          <button class="btn btn-primary w-full m-5">
+            <span v-if="isSubmitting" class="loading loading-spinner"
+              >loading</span
+            >
+            <span v-else>Sign up</span>
           </button>
         </div>
-      </div>
+      </Form>
     </div>
   </div>
 </template>
