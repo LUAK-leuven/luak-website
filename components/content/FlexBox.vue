b905e843e124be1c1a4b25efa68d5569e0a4934a
cedricDsmet <cedric.de.smet@outlook.com>
2024-08-28 13:57:09 +0000

    fix flexbox

--- components/content/FlexBox.vue
+++ components/content/FlexBox.vue
@@ -1,14 +1,42 @@
+<script lang="ts" setup>
+defineProps({
+  justify: {
+    type: String,
+    default: "justify-center",
+  },
+});
+</script>
 <template>
-  <div class="flex flex-row justify around">
-    <ContentSlot v-if="$slots.slot1" :use="$slots.slot1" />
-    <ContentSlot v-if="$slots.slot2" :use="$slots.slot2" />
-    <ContentSlot v-if="$slots.slot3" :use="$slots.slot3" />
-    <ContentSlot v-if="$slots.slot4" :use="$slots.slot4" />
-    <ContentSlot v-if="$slots.slot5" :use="$slots.slot5" />
-    <ContentSlot v-if="$slots.slot6" :use="$slots.slot6" />
-    <ContentSlot v-if="$slots.slot7" :use="$slots.slot7" />
-    <ContentSlot v-if="$slots.slot8" :use="$slots.slot8" />
-    <ContentSlot v-if="$slots.slot9" :use="$slots.slot9" />
-    <ContentSlot v-if="$slots.slot10" :use="$slots.slot10" />
+  <div class="flex flex-wrap w-full" :class="justify">
+    <div v-if="$slots.item1">
+      <ContentSlot key="slot1" :use="$slots.item1" />
+    </div>
+    <div v-if="$slots.item2">
+      <ContentSlot key="slot2" :use="$slots.item2" />
+    </div>
+    <div v-if="$slots.item3">
+      <ContentSlot key="slot3" :use="$slots.item3" />
+    </div>
+    <div v-if="$slots.item4">
+      <ContentSlot key="slot4" :use="$slots.item4" />
+    </div>
+    <div v-if="$slots.item5">
+      <ContentSlot key="slot5" :use="$slots.item5" />
+    </div>
+    <div v-if="$slots.item6">
+      <ContentSlot key="slot6" :use="$slots.item6" />
+    </div>
+    <div v-if="$slots.item7">
+      <ContentSlot key="slot7" :use="$slots.item7" />
+    </div>
+    <div v-if="$slots.item8">
+      <ContentSlot key="slot8" :use="$slots.item8" />
+    </div>
+    <div v-if="$slots.item9">
+      <ContentSlot key="slot9" :use="$slots.item9" />
+    </div>
+    <div v-if="$slots.item10">
+      <ContentSlot key="slot10" :use="$slots.item10" />
+    </div>
   </div>
 </template>
