900676112c84325a60e5eebcdc6e84bb472df371
cedricDsmet <cedric.de.smet@outlook.com>
2024-08-30 03:22:39 +0000

    add sentry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,14 @@ export default defineNuxtConfig({
       anchorLinks: false,
     },
   },
+  sentry: {
+    sourceMapsUploadOptions: {
+      org: "luak",
+      project: "luak-nuxt",
+      authToken:
+        "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",
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxt/content",
@@ -44,5 +52,6 @@ export default defineNuxtConfig({
     "@nuxtjs/supabase",
     "@vee-validate/nuxt",
     "@nuxt/eslint",
+    "@sentry/nuxt/module",
   ],
 });
