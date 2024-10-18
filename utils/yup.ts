ae9ea5235f5645f306ce89949abbc6ae3db4b6ee
Cedric De Smet <Cedric.de.smet@outlook.com>
2024-10-18 11:11:44 +0200

    change phoneRegExp

--- utils/yup.ts
+++ utils/yup.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-const phoneRegExp = /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/;
+const phoneRegExp = /^\+((?:9[679]|8[035789]|6[789]|5[90]|42|3[578]|2[1-689])|9[0-58]|8[1246]|6[0-6]|5[1-8]|4[013-9]|3[0-469]|2[70]|7|1)(?:\W*\d){0,13}\d$/;
 
 const yup_phone = yup.string().matches(
   phoneRegExp,
