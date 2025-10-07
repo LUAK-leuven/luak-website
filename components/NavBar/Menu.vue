0b2623da241478888719917ec714df408176f80f
Cedric De Smet <Cedric.de.smet@outlook.com>
2025-10-07 19:39:17 +0200

    fix closing drawer bug

--- components/NavBar/Menu.vue
+++ components/NavBar/Menu.vue
@@ -6,32 +6,52 @@ const { data: pages_navigation } = await useAsyncData("pages_navigation", () =>
   fetchContentNavigation(queryContent("/pages")),
 );
 const user = useSupabaseUser();
+
+const closeDrawer = () => {
+  const drawerToggle = document.getElementById(
+    "my-drawer-3",
+  ) as HTMLInputElement;
+  if (drawerToggle) {
+    drawerToggle.checked = false;
+  }
+};
 </script>
 <template>
   <ul class="menu">
-    <li><NuxtLink to="/news">News</NuxtLink></li>
-    <li><NuxtLink to="/activities">Activities</NuxtLink></li>
+    <li><NuxtLink to="/news" @click="closeDrawer">News</NuxtLink></li>
+    <li>
+      <NuxtLink to="/activities" @click="closeDrawer">Activities</NuxtLink>
+    </li>
     <li>
       <details>
         <summary>Info</summary>
         <ul class="p-2 bg-base-200 rounded-t-none">
-          <li v-for="nav in info_navigation[0].children" :key="nav._path">
-            <NuxtLink :to="nav._path">{{ nav.title }}</NuxtLink>
+          <li v-for="nav in info_navigation?.[0]?.children" :key="nav._path">
+            <NuxtLink :to="nav._path" @click="closeDrawer">{{
+              nav.title
+            }}</NuxtLink>
           </li>
         </ul>
       </details>
     </li>
-    <li v-for="nav in pages_navigation[0].children" :key="nav._path">
-      <NuxtLink :to="nav._path">{{ nav.title }}</NuxtLink>
+    <li v-for="nav in pages_navigation?.[0]?.children" :key="nav._path">
+      <NuxtLink :to="nav._path" @click="closeDrawer">{{ nav.title }}</NuxtLink>
     </li>
-    <li v-if="user"><NuxtLink to="/stories">Stories</NuxtLink></li>
+    <li v-if="user">
+      <NuxtLink to="/stories" @click="closeDrawer">Stories</NuxtLink>
+    </li>
     <NuxtLink
+      class="btn btn-primary btn-outline"
       v-if="!user"
       to="/profile/overview"
-      class="btn btn-primary btn-outline"
+      @click="closeDrawer"
       >Log In</NuxtLink
     >
-    <NuxtLink v-else to="/profile/overview" class="btn btn-primary btn-outline"
+    <NuxtLink
+      class="btn btn-primary btn-outline"
+      v-else
+      to="/profile/overview"
+      @click="closeDrawer"
       >My Profile</NuxtLink
     >
   </ul>
