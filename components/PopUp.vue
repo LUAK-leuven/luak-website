9cc35a65c80dbd97c46d65d06ac21021952d786f
hektor ltb <hektor.ltb@gmail.com>
2025-11-11 16:28:42 +0000

    implemented the pwd reset flow

---
+++ components/PopUp.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const { show } = defineProps({
+    show: {
+      type: Boolean,
+      required: true,
+    },
+  });
+</script>
+
+<template>
+  <div
+    v-if="show"
+    class="alert alert-success fixed z-50 w-[90%] mx-[5%] md:w-[80%] md:mx-[10%] bottom-0 mb-5 flex justify-between"
+    role="alert">
+    <slot />
+    <button
+      class="btn btn-sm btn-circle btn-ghost text-xl self-center items-end">
+      ✕
+    </button>
+  </div>
+</template>
--- pages/passwordReset.vue
+++ pages/passwordReset.vue
@@ -2,56 +2,106 @@
   import * as yup from 'yup';
 
   const supabase = useSupabaseClient();
+  const route = useRoute();
 
-  supabase.auth.onAuthStateChange(async (event, session) => {
-    void session;
-    console.log('auth change: ', event);
-    if (event == 'PASSWORD_RECOVERY') {
-      console.log('pwd reset event');
-      // const newPassword = prompt(
-      //   'What would you like your new password to be?',
-      // );
-      // const { data, error } = await supabase.auth.updateUser({
-      //   password: newPassword ?? undefined,
-      // });
-      // if (data) alert('Password updated successfully!');
-      // if (error) alert('There was an error updating your password.');
-    }
-  });
+  const dialog = ref<HTMLDialogElement>();
+  const authError = ref<{ error: string; description: string }>();
+  const success = ref(false);
 
   const formSchema = yup.object({
-    newPassword: yup.string().required().label('New password'),
+    pwd1: yup_password.required().label('password'),
+    pwd2: yup_password
+      .required()
+      .oneOf([yup.ref('pwd1')], 'Password does not match')
+      .label('password'),
   });
 
   const { handleSubmit } = useForm({
-    validationSchema: toTypedSchema(formSchema),
-    initialValues: { newPassword: '' },
+    validationSchema: toTypedSchema(formSchema, { abortEarly: false }),
+    initialValues: { pwd1: '', pwd2: '' },
   });
 
   const onSubmit = handleSubmit(async (submitted) => {
-    console.log(`reset pwd to: ${submitted.newPassword}`);
+    success.value = true;
+    console.log(`reset pwd to: ${submitted.pwd1}`);
     const { data, error } = await supabase.auth.updateUser({
-      password: submitted.newPassword,
+      password: submitted.pwd1,
     });
     console.log('data: ', data);
     console.log('error: ', error);
-    if (data.user) alert('Password updated successfully!');
-    if (error) alert('There was an error updating your password.');
+    if (data.user) {
+      success.value = true;
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await navigateTo({
+        path: '/profile/overview',
+      });
+    }
+    if (error) {
+      console.log(error);
+      authError.value = {
+        error: `There was an error updating your password!`,
+        description: error.message,
+      };
+    }
+  });
+
+  effect(() => {
+    if (authError.value) {
+      if (dialog.value === undefined) alert(authError.value.error);
+      else dialog.value.showModal();
+    }
+  });
+
+  onMounted(async () => {
+    dialog.value = document.getElementById('auth-error') as HTMLDialogElement;
+
+    console.log('query:', route.query);
+    if (route.query.error) {
+      authError.value = {
+        error: route.query.error as string,
+        description: route.query.error_description as string,
+      };
+    }
+    if (!route.query.code) {
+      authError.value = {
+        error: 'Redirect link is invalid',
+        description: '',
+      };
+    }
   });
 </script>
+
 <template>
-  <div class="relative flex flex-wrap justify-center bg-base-300">
+  <div class="relative flex justify-center bg-base-300">
     <div
-      class="bg-base-100 shadow-md rounded-lg w-10/12 lg:w-8/12 xl:w-1/3 mb-28 z-10 mt-8 p-5 sm:p-20">
+      class="bg-base-100 shadow-md rounded-lg w-10/12 lg:w-8/12 xl:w-1/3 max-w-[400px] mb-28 z-10 mt-8 p-10">
       <form @submit="onSubmit">
         <InputText
           label="Enter your new password:"
-          name="newPassword"
-          placeholder="new password"></InputText>
+          name="pwd1"
+          placeholder="******"
+          type="password"></InputText>
+        <InputText
+          label="Confirm your password:"
+          name="pwd2"
+          placeholder="******"
+          type="password"></InputText>
         <div class="flex justify-end">
-          <button class="btn btn-primary">Reset</button>
+          <button class="btn btn-primary">Submit</button>
         </div>
       </form>
     </div>
+
+    <dialog id="auth-error" class="modal">
+      <div class="modal-box">
+        <h2>Error: {{ authError?.error }}</h2>
+        <p>{{ authError?.description }}</p>
+        <div class="flex flex-row justify-end">
+          <NuxtLink class="btn btn-primary" to="./">OK</NuxtLink>
+        </div>
+      </div>
+    </dialog>
+
+    <pop-up :show="success">Password updated successfully</pop-up>
   </div>
 </template>
