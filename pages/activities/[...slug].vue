cf1a657fe8a822d30229e41a00c9218cbd08eff1
hektor ltb <hektor.ltb@gmail.com>
2026-01-29 17:38:54 +0000

    fix download PDF issues for rope solo lessons

--- pages/activities/[...slug].vue
+++ pages/activities/[...slug].vue
@@ -10,33 +10,29 @@
 </script>
 
 <template>
-  <template v-if="page">
-    <PagesFullPageCardWithPicture :image-url="page.image">
-      <template #title>
-        <div class="max-w-[90wh]">
-          <h1 class="mb-14 text-center text-5xl">
-            {{ page.title }}
-          </h1>
-          <i>{{
-            new Date(page.date).toLocaleDateString(undefined, {
-              weekday: 'long',
-              day: 'numeric',
-              month: 'long',
-              year: 'numeric',
-            })
-          }}</i>
-        </div>
-      </template>
+  <PagesFullPageCardWithPicture v-if="page" :image-url="page.image">
+    <template #title>
+      <div class="max-w-[90wh]">
+        <h1 class="mb-14 text-center text-5xl">
+          {{ page.title }}
+        </h1>
+        <i>{{
+          new Date(page.date).toLocaleDateString(undefined, {
+            weekday: 'long',
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          })
+        }}</i>
+      </div>
+    </template>
 
-      <PageCard>
-        <NuxtLink
-          class="btn btn-circle btn-sm btn-outline mb-5"
-          to="/activities">
-          <span class="material-symbols-outlined"> arrow_back</span>
-        </NuxtLink>
-        <ContentRenderer class="nuxt-content" :value="page" />
-      </PageCard>
-    </PagesFullPageCardWithPicture>
-  </template>
-  <PagesPageNotFound v-else />
+    <PageCard>
+      <NuxtLink class="btn btn-circle btn-sm btn-outline mb-5" to="/activities">
+        <span class="material-symbols-outlined"> arrow_back</span>
+      </NuxtLink>
+      <ContentRenderer class="nuxt-content" :value="page" />
+    </PageCard>
+  </PagesFullPageCardWithPicture>
+  <PagesPageNotFound v-else class="mt-20" />
 </template>
