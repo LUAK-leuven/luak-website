d85e7c98322280250e8f3070d93dd32c978bb366
hektor ltb <hektor.ltb@gmail.com>
2026-03-22 17:03:29 +0000

    Finished topo details page

--- pages/topos/[id].vue
+++ pages/topos/[id].vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  import type { Enums } from '~/types/database.types';
   import type { TopoId } from '~/types/gear';
 
   definePageMeta({ middleware: 'active-member-guard' });
@@ -6,6 +7,13 @@
   const { data: user } = await useLuakMember();
   const topoId = useRoute().params.id as TopoId;
   const { data: topo, pending } = await gearService().getTopo(topoId);
+
+  const conditionMap: Record<Enums<'topo_condition'>, string> = {
+    as_good_as_new: 'New',
+    good: 'Good',
+    used: 'Used',
+    damaged: 'Damaged',
+  };
 </script>
 
 <template>
@@ -27,16 +35,43 @@
       <span class="loading loading-spinner loading-lg" />
     </div>
     <div v-else-if="!topo">ERROR!</div>
-    <div v-else class="grid">
-      <p>Amount: {{ topo.amount }}</p>
-      <p v-if="user.isBoard">Condition: {{ topo.condition }}</p>
-      <p>Countries: {{ topo.countries.join(', ') }}</p>
-      <p>Details: {{ topo.details }}</p>
-      <p>Languages: {{ topo.languages.join(', ') }}</p>
-      <p v-if="user.isBoard">Place in library: {{ topo.place_in_library }}</p>
-      <p>Tags: {{ topo.tags.join(', ') }}</p>
-      <p>Types of climbing: {{ topo.types_of_climbing.join(', ') }}</p>
-      <p>Year published: {{ topo.year_published }}</p>
+    <div
+      v-else
+      class="grid grid-cols-1 sm:grid-cols-2 gap-x-2 gap-y-3 mt-2 mb-4">
+      <p class="col-span-2">{{ topo.details }}</p>
+      <TopoLibraryTopoDetailItem name="Types of climbing">
+        <span>{{ topo.types_of_climbing.join(', ') }}</span>
+      </TopoLibraryTopoDetailItem>
+      <TopoLibraryTopoDetailItem name="Countries">
+        <span>{{ topo.countries.join(', ') }}</span>
+      </TopoLibraryTopoDetailItem>
+      <TopoLibraryTopoDetailItem name="Condition">
+        <span
+          class="badge"
+          :class="{
+            'badge-success':
+              topo.condition === 'as_good_as_new' || topo.condition === 'good',
+            'badge-warning': topo.condition === 'used',
+            'badge-error': topo.condition === 'damaged',
+          }">
+          {{ conditionMap[topo.condition] }}
+        </span>
+      </TopoLibraryTopoDetailItem>
+      <TopoLibraryTopoDetailItem name="Amount">
+        <span class="badge badge-ghost">{{ topo.amount }}</span>
+      </TopoLibraryTopoDetailItem>
+      <TopoLibraryTopoDetailItem name="Languages">
+        <span>{{ topo.languages.join(', ') }}</span>
+      </TopoLibraryTopoDetailItem>
+      <TopoLibraryTopoDetailItem name="Place in library">
+        <span>{{ topo.place_in_library }}</span>
+      </TopoLibraryTopoDetailItem>
+      <TopoLibraryTopoDetailItem name="Tags">
+        <span>{{ topo.tags.join(', ') }}</span>
+      </TopoLibraryTopoDetailItem>
+      <TopoLibraryTopoDetailItem name="Year published">
+        <span class="badge badge-info">{{ topo.year_published }}</span>
+      </TopoLibraryTopoDetailItem>
     </div>
     <template v-if="user.isBoard">
       <hr />
