08cfa5413d3416c47ddb5a210e780462a8fd0382
hektor ltb <hektor.ltb@gmail.com>
2026-03-23 20:23:35 +0000

    Added QR code generator

---
+++ pages/qr-test.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { toCanvas } from 'qrcode';
+
+  const canvasRef = ref<HTMLCanvasElement | null>(null);
+
+  onMounted(async () => {
+    if (!canvasRef.value) {
+      return;
+    }
+
+    await toCanvas(canvasRef.value, 'Hello world!', {
+      errorCorrectionLevel: 'M',
+      width: 220,
+      margin: 1,
+    });
+  });
+</script>
+
+<template>
+  <FullPageCard>
+    <template #title>Test</template>
+    <canvas ref="canvasRef" />
+  </FullPageCard>
+</template>
