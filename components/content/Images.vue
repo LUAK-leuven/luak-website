352a3ffd34649d7d3c89a0a7ccc5f0d06c8a5f1b
hektor ltb <hektor.ltb@gmail.com>
2026-07-25 22:43:59 +0200

    Sialouze story

---
+++ components/content/Images.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  defineProps<{
+    src: string[];
+    caption?: string | undefined;
+  }>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <div class="flex flex-row flex-wrap justify-center gap-4">
+      <NuxtImg v-for="(image, idx) of src" :key="idx" :src="image" />
+    </div>
+    <p v-if="caption">
+      <em>{{ caption }}</em>
+    </p>
+  </div>
+</template>
