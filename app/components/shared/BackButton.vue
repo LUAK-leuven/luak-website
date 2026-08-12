1b3e31aecb3746e2df3594f7fa9fa15da93209c1
hektor ltb <hektor.ltb@gmail.com>
2026-08-12 15:44:37 +0200

    Fixed back button for topo library

--- app/components/shared/BackButton.vue
+++ app/components/shared/BackButton.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
   import type { RoutesNamedLocations } from '@typed-router';
+  import Button from './Button.vue';
 
-  defineProps<{ to: RoutesNamedLocations }>();
+  defineProps<{ to: RoutesNamedLocations | undefined }>();
+
+  const router = useRouter();
 </script>
 <template>
-  <NuxtLink class="btn btn-circle btn-sm btn-outline" :to="to">
+  <Button
+    v-if="to === undefined"
+    class="btn btn-circle btn-sm btn-outline"
+    @click="() => router.back()">
+    <span class="material-symbols-outlined">arrow_back</span>
+  </Button>
+  <NuxtLink v-else class="btn btn-circle btn-sm btn-outline" :to="to">
     <span class="material-symbols-outlined">arrow_back</span>
   </NuxtLink>
 </template>
