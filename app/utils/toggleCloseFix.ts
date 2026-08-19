9ace7d0ef6223b8527b2ca11ef97abfbe296bcbf
hektor ltb <hektor.ltb@gmail.com>
2026-08-19 12:35:59 +0200

    Add 55y logo

--- app/utils/toggleCloseFix.ts
+++ app/utils/toggleCloseFix.ts
@@ -4,9 +4,6 @@ export const toggleCloseFix = (templateRef: Ref<HTMLDetailsElement | null>) => {
   const summaryElement = computed(
     () => templateRef.value?.querySelector('summary') ?? undefined,
   );
-  if (summaryElement.value === undefined) {
-    console.warn(`toggleCloseFix: summaryElement is undefined`);
-  }
 
   onClickOutside(summaryElement, () => {
     closeToggle(templateRef);
